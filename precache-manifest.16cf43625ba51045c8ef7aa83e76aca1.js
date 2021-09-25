21d488d6df4fd9c9ecf0e180cde478de806e27cc
Shajia Abidi <sabidi@pewresearch.org>
2021-09-25 16:25:59 -0500

    Updates

---
+++ precache-manifest.16cf43625ba51045c8ef7aa83e76aca1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5a0d3d6af7fb9d597c0f",
+    "url": "/abidishajia/static/css/main.cc4771bc.chunk.css"
+  },
+  {
+    "revision": "5a0d3d6af7fb9d597c0f",
+    "url": "/abidishajia/static/js/main.5a0d3d6a.chunk.js"
+  },
+  {
+    "revision": "37be6123f7f1a56fb8da",
+    "url": "/abidishajia/static/css/1.6425679e.chunk.css"
+  },
+  {
+    "revision": "37be6123f7f1a56fb8da",
+    "url": "/abidishajia/static/js/1.37be6123.chunk.js"
+  },
+  {
+    "revision": "ce175004eaa4e88d3799",
+    "url": "/abidishajia/static/js/runtime~main.ce175004.js"
+  },
+  {
+    "revision": "6551f87df8fda3f5436bb188ff5be941",
+    "url": "/abidishajia/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.91586e94817a3aa0b91ab048d610f3c6.js"
+  "/abidishajia/precache-manifest.16cf43625ba51045c8ef7aa83e76aca1.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/index.html", {
+workbox.routing.registerNavigationRoute("/abidishajia/index.html", {
   
   blacklist: [/^\/_/,/\/[^/]+\.[^/]+$/],
 });
